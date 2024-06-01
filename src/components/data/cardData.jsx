7a0986b2beff702a3be1c127fc46eaa3a4ffce2a
sysys10 <166797095+sysys10@users.noreply.github.com>
2024-06-02 04:58:50 +0900

    Add files via upload

---
+++ src/components/data/cardData.jsx
@@ -0,0 +1,40 @@
+const cardData = [
+    {
+      link: "http://syspclone1234.s3-website.ap-northeast-2.amazonaws.com/",
+      bg_img: "forif_bg_img",
+      title: "포리프 클론 코딩",
+      desc: "이 작업을 통하여 tailwind css의 사용법과 useState, useEffect등의 기본적인 리액트 문법을 배웠다. (반응형X)",
+      bottom_link: "https://github.com/sysys10/Forif_clone",
+      bottom_desc: "깃허브 링크",
+      date: "24.05.18 ~ 25",
+    },
+    {
+      link: "https://www.inflearn.com/course/css-flex-grid-%EC%A0%9C%EB%8C%80%EB%A1%9C-%EC%9D%B5%ED%9E%88%EA%B8%B0/dashboard",
+      bg_img: "infren_bg",
+      title: '"CSS Flex와 Grid 제대로 익히기" 강의 수강.',
+      desc: "이 강의로 CSS flex에 대한 기본적인 이해를 하였다. 개념은 벨로그에 정리하였고, 포트폴리오를 만들며 복습을 하였다.",
+      bottom_link:
+        "https://velog.io/@ys10/CSS-Flex%EC%99%80-Grid-%EC%A0%9C%EB%8C%80%EB%A1%9C-%EC%9D%B5%ED%9E%88%EA%B8%B0",
+      bottom_desc: "벨로그 링크",
+      date: "24.05.30 ~ 31",
+    },
+    {
+      link: "",
+      bg_img: "portafolio-bg",
+      title: "포트폴리오 제작",
+      desc: "이 포트폴리오를 제작하면서 CSS Flex 복습과 반응형, Figma등 웹 개발에 필요한 요소들을 공부하였고 AWS배포를 해보았다. (반응형O)",
+      bottom_link: "https://github.com/sysys10/Portafolio_1",
+      bottom_desc: "깃허브 링크",
+      date: "24.05.31 ~ 06.02",
+    },
+    {
+      link: "",
+      bg_img: "",
+      title: "",
+      desc: "앞으로 채워나가겠습니다!",
+      bottom_link: "",
+      bottom_desc: "",
+      date: "",
+    }
+];
+  export default cardData
---
+++ src/components/data/introdutionData.jsx
@@ -0,0 +1,19 @@
+const introdutionData = [
+    {
+      title: "html css javascript",
+      desc: "html css js를 사용하여 여러 부트스트랩 예제들을 풀어본 경험이있으며, 기본적인 속성들에 keyframe을 사용한 애니메이션, media를 사용한 반응형등의 css 속성들을 사용할 줄 안다.",
+    }, {
+      title: "Tailwind css",
+      desc: "tailwind css를 사용하여 클론 코딩, 포트폴리오를 만들었으며 flex와 sm, md, lg, % 를 이용한 반응형웹과 tailwindcss의 애니메이션 등을 사용할 수 있다.",
+    },
+    {
+      title: "React",
+      desc: "리액트의 useState, useRef, 컴포넌트와 JS의 map, 배열등의 문법을 알고 웹개발에 사용할 수 있다. 리액트를 사용하여 클론 코딩, 포트폴리오등을 만들어보았다. 공부한 기간이 얼마 되지않아 문법의 능숙한 사용은 어렵다.",
+    },
+   
+    {
+      title: "BackEnd",
+      desc: "jsExpress를 이용한 라우팅, html 서빙등을 할 줄 알고, MySQL의 테이블 만들기, 문법들을 공부하였다. 또한 NodeJS의 Sequelize 라이브러리를 사용하여 SQL의 기본적인 활용을 해보았다. 아직 배우는 중이다.",
+    },
+];
+  export default introdutionData
