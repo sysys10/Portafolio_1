9834f50215b5e2d23d4f34ff6366cd0beed387fd
sysys10 <166797095+sysys10@users.noreply.github.com>
2024-06-04 12:58:10 +0900

    Delete src/components directory

--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-function footer() {
-  return (
-    <div className="w-full h-40 flex text-sm flex-col text-center azurlane justify-center">
-          <p>© 2024 신윤수</p>
-        <a href="mailto:yunsu102896@gmail.com"> contact: yunsu102896@gmail.com</a>
-    </div>
-  );
-}
-export default footer;
